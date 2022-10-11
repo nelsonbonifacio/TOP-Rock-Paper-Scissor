aeca1aac300f4403544e3c469131201d065c8c00
Nelson Bonifacio <nbonifacio04@gmail.com>
2022-10-11 17:28:45 -0500

    Created a function called getComputerChoice to get a choice of rock paper or scissors

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  const randomNum = Math.floor(Math.random() * 3) + 1;
+  if (randomNum === 1) {
+    console.log("Rock");
+  } else if (randomNum === 2) {
+    console.log("Paper");
+  } else {
+    console.log("Scissor");
+  }
+}
+getComputerChoice();
