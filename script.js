266764ef15e94a4329b66c6ed7514e088a78949e
Nelson Bonifacio <nbonifacio04@gmail.com>
2022-10-11 17:57:21 -0500

    added roundOfRPS function to simulate 1 round of rock, paper, scissors

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   if (randomNum === 1) {
-    console.log("Rock");
+    return "Rock";
   } else if (randomNum === 2) {
-    console.log("Paper");
+    return "Paper";
   } else {
-    console.log("Scissor");
+    return "Scissors";
   }
 }
-getComputerChoice();
+
+function roundOfRPS(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+  console.log(playerSelection, computerSelection);
+  if (playerSelection == "Rock" && computerSelection == "Paper") {
+    return `you Lose, ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return `You Win, ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return `you Lose, ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return `You Win, ${playerSelection} beats ${computerSelection}`;
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return `you Lose, ${computerSelection} beats ${playerSelection}`;
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return `You Win, ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return "It's a tie";
+  }
+}
+
+console.log(roundOfRPS("Rock", getComputerChoice()));
