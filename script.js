d161bd2a8cc92c35dcd6e59fe7f86e5ec3105cec
Nelson Bonifacio <nbonifacio04@gmail.com>
2022-10-11 18:44:34 -0500

    Created a function called game to simulate five rounds of rock, paper, scissors and find the winner

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   if (randomNum === 1) {
@@ -12,22 +15,44 @@ function getComputerChoice() {
 function roundOfRPS(playerSelection, computerSelection) {
   playerSelection =
     playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-  console.log(playerSelection, computerSelection);
-  if (playerSelection == "Rock" && computerSelection == "Paper") {
+
+  if (
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    computerScore++;
     return `you Lose, ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    playerScore++;
     return `You Win, ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    return `you Lose, ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    return `You Win, ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-    return `you Lose, ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    return `You Win, ${playerSelection} beats ${computerSelection}`;
-  } else {
+  } else if (playerSelection === computerSelection) {
     return "It's a tie";
+  } else {
+    return "invalid input. Try again";
   }
 }
 
-console.log(roundOfRPS("Rock", getComputerChoice()));
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(
+      roundOfRPS(
+        prompt("please enter your choice of Rock, Paper, or Scissors"),
+        getComputerChoice()
+      )
+    );
+  }
+  if (playerScore > computerScore) {
+    return `congratulation you beat the CPU ${playerScore}-${computerScore}`;
+  } else if (playerScore < computerScore) {
+    return `Damn you lost to the CPU ${playerScore}-${computerScore}`;
+  } else {
+    return `You tied with the CPU ${playerScore}-${computerScore}`;
+  }
+}
+
+console.log(game());
